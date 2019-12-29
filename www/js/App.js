bec9b2210181d698a7fe0ecbf4409b24ac47f531
Martyna Roj <martynaxroj@gmail.com>
2019-12-29 02:07:04 +0100

    [MODIFY] divide main script to app class

---
+++ www/js/App.js
@@ -0,0 +1,31 @@
+class App {
+
+    // Application Constructor
+    initialize = () => {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    }
+
+    // deviceready Event Handler
+    onDeviceReady = () => {
+        this.receivedEvent('deviceready');
+    }
+
+    // Update DOM on a Received Event
+    receivedEvent = (id) => {
+        let parentElement = document.getElementById(id);
+        let listeningElement = parentElement.querySelector('.listening');
+        let receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        this.start();
+    }
+
+    //Start menu app
+    start = () => {
+        let menu = new Menu();
+        document.getElementById("muteButton").onclick = menu.muteButton;
+        menu.playMusic();
+    }
+};
--- www/js/index.js
+++ www/js/index.js
@@ -17,28 +17,5 @@
  * under the License.
  */
 
-var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
-
-    // deviceready Event Handler
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
-
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
-};
-
+let app = new App();
 app.initialize();
