970071873bbd60bb2526879dc061bd6d297978e0
Martyna Roj <martynaxroj@gmail.com>
2020-01-05 01:38:59 +0100

    [ADD] rand width of biomes and which of them, procedural adding biomes

--- www/js/World.js
+++ www/js/World.js
@@ -5,25 +5,53 @@ class World {
         this.canvas = canvas;
         this.level = 1;  //how hard world is, firstly 1
 
+        this.tileW = 48;
+        this.tileH = 48;
+        this.tilesCountW = this.canvas.width / this.tileW;
+        this.filledTiles = 0;
+
         this.biomes = [];    //array of biomes: river, street...
         this.init();
     }
 
     init = () => {
-        //loop with adding biome to fill canvas width & height...
-        this.addBiome(1);
+        this.addBiome(0);
+        while (this.filledTiles < this.tilesCountW) {
+            const id = this.rand(1, 3);
+            this.addBiome(id);
+
+            const ifSaveArea = this.rand(0, 1);
+            if (ifSaveArea) this.addBiome(0);
+        }
     }
 
     addBiome = id => {
-        // switch (id) {
-        // case 1:
-            this.biomes.push(new SaveArea(this.assetsLoader.get("SAVE_AREA"), this.canvas.height, this.level));
-        //cases with biomes...
-        //}
+        let biome = null;
+
+        switch (id) {
+        case 0:
+            biome = new SaveArea(this.assetsLoader.get("SAVE_AREA"), this.canvas, this.tileW, this.tileH, this.filledTiles, this.level);
+            break;
+        case 1:
+            biome = new River(this.assetsLoader.get("RIVER"), this.canvas, this.tileW, this.tileH, this.filledTiles, this.level);
+            break;
+        case 2:
+            biome = new Street(this.assetsLoader.get("STREET"), this.canvas, this.tileW, this.tileH, this.filledTiles, this.level);
+            break;
+        case 3:
+            biome = new Sky(this.assetsLoader.get("SKY"), this.canvas, this.tileW, this.tileH, this.filledTiles, this.level);
+            break;
+        }
+        this.filledTiles += biome.getTileCountW();
+        this.biomes.push(biome);
     }
 
     levelUp = () => {
         this.level++;
+    }
+
+    rand = (min, max) => {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
 }
