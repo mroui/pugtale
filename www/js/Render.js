1422319845a0d08b0b309652a4f5cfa3c99fd174
Martyna Roj <martynaxroj@gmail.com>
2020-01-08 00:35:11 +0100

    [MODIFY] render structure & rendering objects to fix respawning not displayed objects

--- www/js/Render.js
+++ www/js/Render.js
@@ -1,12 +1,13 @@
 class Render {
 
-    constructor(canvas)  {
+    constructor(canvas, assetsLoader)  {
         this.canvas = canvas;
         this.context = this.canvas.getContext('2d');
+        this.assetsLoader = assetsLoader;
 
         this.world = null;
+        this.biomes = [];
         this.pug = null;
-        this.objects = [];
 
         this.tileSize = 48;
 
@@ -28,15 +29,50 @@ class Render {
     }
 
     render = () => {
-        this.objects.forEach( object => {
-            this.draw(object);
-        });
+        if (this.biomes !== null && this.pug !== null) {
+
+            let arrayFromRemove = false, indexBiome = 0, index = 0;
+
+            this.biomes.forEach((biome, iB) => {
+
+                biome.getTiles().forEach((tile, iT) => {
+                    if (tile.getIsDisplayed()) this.draw(tile);
+                    else if (tile.getToRemove()) {arrayFromRemove = true; indexBiome = iB; index = iT;}
+                });
+                if (arrayFromRemove) {
+                    let removeObject = this.biomes[indexBiome].getTiles()[index];
+                    this.biomes[indexBiome].getTiles().splice(index, 1);
+                    this.biomes[indexBiome].getTiles().push(this.rerenderPassedObject(indexBiome, removeObject));
+                    arrayFromRemove = false;
+                }
+
+                biome.getObjects().forEach((object, iO) => {
+                    if (object.getIsDisplayed()) this.draw(object);
+                    else if (object.getToRemove()) {arrayFromRemove = true; indexBiome = iB; index = iO;}
+                });
+                if (arrayFromRemove) {
+                    let removeObject = this.biomes[indexBiome].getObjects()[index];
+                    this.biomes[indexBiome].getObjects().splice(index, 1);
+                    this.biomes[indexBiome].getObjects().push(this.rerenderPassedObject(indexBiome, removeObject));
+                    arrayFromRemove = false;
+                }
+
+            });
+            if (this.pug.getIsDisplayed()) this.draw(this.pug);
+        }
+    }
+
+    rerenderPassedObject = (indexBiome, removeObject) => {
+        let newObject = this.biomes[indexBiome].spawnObject(removeObject.getStartY(), removeObject.getX());
+        newObject.setDelay(removeObject.getDelay());
+        newObject.start();
+        return newObject;
     }
 
     draw = object => {
         this.context.drawImage(object.getAsset(), object.getXA(), object.getYA(),
-                               object.getAW(), object.getAH(), object.getX(),
-                               object.getY(), object.getW(), object.getH());
+            object.getAW(), object.getAH(), object.getX(),
+            object.getY(), object.getW(), object.getH());
     }
 
     checkCollision = () => {
@@ -47,10 +83,24 @@ class Render {
         //check if game end
     }
 
-    init = (objects, world, pug) => {
-        this.objects = objects;
+    init = (world) => {
         this.world = world;
-        this.pug = pug;
+        this.world.biomes.forEach(biome => {
+            this.biomes = this.biomes.concat(biome);
+        });
+        this.pug = new Pug(this.assetsLoader.get("PUG"), 0, 0, 48, 48, 0, 0, 48, 48);
+    }
+
+    startObjects = () => {
+        this.pug.start();
+        this.biomes.forEach(biome => {
+            biome.getTiles().forEach(tile => {
+                tile.start();
+            });
+            biome.getObjects().forEach(object => {
+                object.start();
+            })
+        });
     }
 
     start = () => {
--- www/js/River.js
+++ www/js/River.js
@@ -1,42 +1,59 @@
-class River extends Biome {
+class River extends Biome{
 
     constructor(assetsLoader, canvas, tileW, tileH, filledTiles, level) {
         super(assetsLoader.get("RIVER"), canvas, tileW, tileH, filledTiles, level);
 
         this.assetsLoader = assetsLoader;
-        this.level = level;
-
-        this.donuts = [];
 
         this.donutW = tileW;
         this.donutH = tileH;
 
-        this.activeTileX = filledTiles * this.tileW;
+        this.delay = 0;
+        this.renderSpeed = 20;
+
+        this.direction = -this.donutH;
 
         this.initObjects();
     }
 
     initObjects = () => {
         for (let i = 0; i < this.getTilesCountW(); i++) {
-            let direction;
-            if (rand(0, 1)) direction = -this.donutH;
-            else direction = this.canvas.height;
 
-            this.initDonuts(direction);
-            this.addObjects(this.donuts);
+            let min = 6 - this.level;
+            let max = 8 - this.level;
+            if (min == 0) {
+                min = 2;
+                max = 5;
+            }
+
+            let objectsCount = rand(min, max);
+            let direction = this.setDirection();
+
+            for(let i = 0; i < objectsCount; i++) {
+                let object = this.spawnObject(direction, this.activeTileX);
+                this.objects.push(object);
+            }
+
+            this.delay = 0;
             this.activeTileX += this.tileW;
         }
     }
 
-    initDonuts = direction => {
-        let min = this.getTilesCountH() - 2;
-        let max = this.getTilesCountH() + 2;
-        let donutsCount = rand(min, max);
+    setDirection = () => {
+        this.direction = this.direction < 0 ? this.canvas.height : -this.donutH;
+        return this.direction;
+    }
 
-        for (let j = 0; j < donutsCount; j++) {
-            let donut = new GameObject(this.assetsLoader.get("DONUT"), 0, 0, this.donutW, this.donutH, this.activeTileX, direction, this.donutW, this.donutH);
-            this.donuts.push(donut);
-        }
+    spawnObject = (direction, activeTileX) => {
+        this.randDelay();
+        let donut = new Donut(this.assetsLoader.get("DONUT"), 0, 0, this.donutW, this.donutH, activeTileX, direction,       this.donutW, this.donutH, this.renderSpeed, this.delay);
+        return donut;
+    }
+
+    randDelay = () => {
+        let min = 1500;
+        let max = 3000;
+        this.delay += rand(min, max);
     }
 
 }
