5735c8f501267a6f7e2b563de5df5563a0299f00
Martyna Roj <martynaxroj@gmail.com>
2020-01-16 00:47:47 +0100

    [ADD] powerups assets & collect sound

--- www/js/AssetsLoader.js
+++ www/js/AssetsLoader.js
@@ -10,6 +10,7 @@ class AssetsLoader {
         this.loadSound("HIT", getUrl("assets/sounds/hit.wav"), false, false);
         this.loadSound("JUMP", getUrl("assets/sounds/jump.wav"), false, false);
         this.loadSound("GAMEOVER", getUrl("assets/sounds/gameover.wav"), false, false);
+        this.loadSound("COLLECT", getUrl("assets/sounds/collect.wav"), false, false);
         this.loadImage("CAR1", getUrl("assets/images/CAR1.png"));
         this.loadImage("CAR2", getUrl("assets/images/CAR2.png"));
         this.loadImage("CAR3", getUrl("assets/images/CAR3.png"));
@@ -18,6 +19,9 @@ class AssetsLoader {
         this.loadImage("DONUT", getUrl("assets/images/DONUT.png"));
         this.loadImage("CLOUD1", getUrl("assets/images/CLOUD1.png"));
         this.loadImage("CLOUD2", getUrl("assets/images/CLOUD2.png"));
+        this.loadImage("POWERUP1", getUrl("assets/images/POWERUP1.png"));
+        this.loadImage("POWERUP2", getUrl("assets/images/POWERUP2.png"));
+        this.loadImage("POWERUP3", getUrl("assets/images/POWERUP3.png"));
         this.loadImage("SAVE_AREA", getUrl("assets/images/SAVE_AREA.png"));
         this.loadImage("RIVER", getUrl("assets/images/RIVER.png"));
         this.loadImage("SKY", getUrl("assets/images/SKY.png"));
