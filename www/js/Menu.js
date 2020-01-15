8454ebce9493ed7eeb3c32f797164db051fec95f
Martyna Roj <martynaxroj@gmail.com>
2020-01-15 21:10:15 +0100

    [ADD] own set nickname modal with validation

--- www/js/Menu.js
+++ www/js/Menu.js
@@ -35,16 +35,18 @@ class Menu {
 
     setListeners = () => {
         document.getElementById("muteButton").addEventListener("click", this.onclickMuteButton);
-        document.getElementById("menu-play").addEventListener("click", this.openNickModal);
+        document.getElementById("menu-play").addEventListener("click", this.openNicknameModal);
         document.getElementById("menu-credits").addEventListener("click", this.openCreditsModal);
         document.getElementById("menu-highscores").addEventListener("click", this.openHighscoresModal);
         document.getElementById("credits-modal__close").addEventListener("click", this.hideCreditsModal);
         document.getElementById("highscores-modal__close").addEventListener("click", this.hideHighscoresModal);
         document.getElementById("network-modal__close").addEventListener("click", this.hideNetworkModal);
+        document.getElementById("set-nickname-modal__close").addEventListener("click", this.hideNicknameModal);
         window.onclick = (e) => {
             if (e.target == document.getElementById("credits-modal")) this.hideCreditsModal();
             else if (e.target == document.getElementById("highscores-modal")) this.hideHighscoresModal();
             else if (e.target == document.getElementById("network-modal")) this.hideNetworkModal();
+            else if (e.target == document.getElementById("set-nickname-modal")) this.hideNicknameModal();
         }
 
         document.addEventListener("pause", this.muteOnBackground, false);
@@ -99,20 +101,29 @@ class Menu {
         this.sound.play();
     }
 
-    openNickModal = () => {
+    openNicknameModal = () => {
         const nickname = "player" + Math.floor(new Date().getTime() + "" + Math.random()*(999));
-        navigator.notification.prompt(
-            'Please enter your nickname',
-            this.onNicknameConfirm,
-            'Registration',
-            ['Done','Cancel'],
-            nickname
-        );
+        document.getElementById("set-nickname-modal__input").value = nickname;
+        document.getElementById("set-nickname-modal__button").addEventListener("click", this.confirmNickname);
+        document.getElementById("set-nickname-modal").style.display = "flex";
     }
 
-    onNicknameConfirm = results => {
-        if (results.buttonIndex == 1) this.playNewGame(results.input1);
-        else return;
+    hideNicknameModal = () => {
+        document.getElementById("set-nickname-modal").style.display = "none";
+        document.getElementById("set-nickname-modal__button").removeEventListener("click", this.confirmNickname);
+        document.getElementById("set-nickname-modal__error").innerHTML = "";
+    }
+
+    confirmNickname = () => {
+        let nickname = document.getElementById("set-nickname-modal__input").value;
+        if (nickname.length < 3 || nickname.length > 22) {
+            document.getElementById("set-nickname-modal__error").style.color = "red";
+            document.getElementById("set-nickname-modal__error").innerHTML = "Nickname must be 3-22 length."
+        }
+        else {
+            this.hideNicknameModal();
+            this.playNewGame(nickname);
+        }
     }
 
     playNewGame = playerNickname => {
